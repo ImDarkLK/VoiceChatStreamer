1fe194f77a79d40077ceb389c35b1b7e3473b5cb
ImDarkLK <86813581+ImDarkLK@users.noreply.github.com>
2021-09-15 16:19:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# PLEASE EDIT THIS FILE IF YOU DEPLOYING THIS BOT ON VPS. ELSE *DON'T* TOUCH THIS FILE.
+
+# Please edit 'CONFIG = False' to 'CONFIG = True'
+CONFIG = True
+
+# Add your APP ID from my.telegram.org
+API_ID = 1552022
+
+# Add your API HASH from my.telegram.org
+API_HASH = "49e5275cda588b56b1e5136f3ef3c632"
+
+# Read readme and generate string session. Then add that to here.
+SESSION = "BQBq7m7CuFoeVPunQQgLtZSnZBLv85f18LmPcMZWn0apGwTHkQMplCX2YMtAcIpXvLsm8XydbuZOb-wfxUaQoVL6fqN_Gpu1ujpwmlUp226LzNXW97Wz4nkzCkTvYFEhW8PTUgbMuW9iw0_9yQPSOCdEaDfAs1r14Vnp_i0Kj7eYcD2Mjmq14E94U6gAoTusvl5-bjziVX288EmUsBBG07sXnEIcZcwPTBCGUV4_jNvHnr7OnwoCVLI_xcoBAduPmRsE4niJ6yYFkLLOSZ9lJ5Gdu6JoLL3jrXL_If45rTXMx9aZTvWzSQ6n6LJJcq_vEotzPSaPdoAsnHI4KzfIGsVDSaZvtwA"
+
+# Add your bot token here
+TOKEN = "1921906120:AAHFBRG_ng0m7C8BA_eQ0JIeoafHnJsTbZA"
+
+# Chat id where bot should work.
+CHAT_ID = -1001520980435
+
+# Add ur TG ID, Seperate by spaces
+ADMINS = 1277798523
